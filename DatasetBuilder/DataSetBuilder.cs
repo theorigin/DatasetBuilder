6ba15bc1c4c22db8cf2c5d67cb81656f614d3c4c
andyrobinson@hotmail.com <andyrobinson@hotmail.com>
2014-12-18 17:27:39 +0000

    Initial commit

---
+++ DatasetBuilder/DataSetBuilder.cs
@@ -0,0 +1,51 @@
+﻿namespace Viewsource.DatasetBuilder
+{
+    using System;
+    using System.Data;
+
+    public class DataSetBuilder
+    {
+        private readonly DataSet dataSet = new DataSet();
+
+        public DataSetBuilder AddDataTable(string tableName)
+        {
+            dataSet.Tables.Add(new DataTable(tableName));
+
+            return this;
+        }
+
+        public DataSetBuilder AddColumn(string columnName)
+        {
+            AddColumn(columnName, typeof(string));
+
+            return this;
+        }
+
+        public DataSetBuilder AddColumn(string columnName, Type type)
+        {
+            var datatable = dataSet.Tables[dataSet.Tables.Count - 1];
+
+            datatable.Columns.Add(new DataColumn(columnName, type));
+
+            return this;
+        }
+
+        public DataSetBuilder AddRow(params object[] row)
+        {
+            var datatable = dataSet.Tables[dataSet.Tables.Count - 1];
+            var dataRow = datatable.NewRow();
+
+            for (var i = 0; i <= row.GetUpperBound(0); i++)
+                dataRow[i] = row[i];
+
+            datatable.Rows.Add(dataRow);
+
+            return this;
+        }
+
+        public DataSet Build()
+        {
+            return dataSet;
+        }
+    }
+}
---
+++ Tests/DataSetBuilder.Tests.cs
@@ -0,0 +1,41 @@
+﻿namespace Viewsource.DatasetBuilder.Tests
+{
+    using System;
+    using System.Data;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DataSetBuilderTests
+    {
+        [Test(Description = "Correct tables, columns and data generated")]
+        public void CorrectDatasetReturned()
+        {
+            var datasetBuilder =
+                new DataSetBuilder().AddDataTable("Users")
+                    .AddColumn("Id", typeof(int))
+                    .AddColumn("Firstname")
+                    .AddColumn("Lastname")
+                    .AddColumn("DateOfBirth", typeof(DateTime))
+                    .AddColumn("IsActive", typeof(bool))
+                    .AddDataTable("Logins")
+                    .AddColumn("Id", typeof(Guid))
+                    .AddColumn("UserId", typeof(int))
+                    .AddColumn("LoginDate", typeof(DateTime));
+
+            var result = datasetBuilder.Build();
+
+            Assert.That(result.Tables.Count, Is.EqualTo(2));
+            Assert.That(result.Tables[0].TableName, Is.EqualTo("Users"));
+            Assert.That(result.Tables[1].TableName, Is.EqualTo("Logins"));
+        }
+
+        [Test(Description = "If nothing is setup on the builder expect an empty dataset to be returned")]
+        public void EmptyDatasetReturnedWhenNoSetupPerformed()
+        {
+            var datasetBuilder = new DataSetBuilder();
+
+            Assert.IsInstanceOf<DataSet>(datasetBuilder.Build());
+        }
+    }
+}
